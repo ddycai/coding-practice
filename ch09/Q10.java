a8240c1656a08dbb9c520f7bee4e975416c17a6a
Duncan Cai <dcai5@uwo.ca>
2014-12-26 23:17:56 -0500

    Problems for Dec 26

--- ch09/Q10.java
+++ ch09/Q10.java
@@ -1,20 +1,120 @@
 /*
- * You have a stack of n boxes, with widths w, heights l and depths d. The boxes
+ * You have a stack of n boxes, with widths w, heights h and depths d. The boxes
  * cannot be rotated and can only be stacked on top of one another if each box in 
  * the stack is strictly larger than the box above it in width, height, and depth.
- * Implement a method to build the tallest stack possible, where the heigh t of a
+ * Implement a method to build the tallest stack possible, where the height of a
  * stack is the sum of the heights of each box. 
+ *
+ * Solution
+ * Let each box be the bottom and construct the best stack with that box on the
+ * bottom, then take the max of all the bottoms. How do we construct the best
+ * stack with bottom b[i]? The same way, we take each box as the bottom that
+ * can fit on top of b[i] and build the best stack, then we take the maximum of
+ * those and so on.
+ *
+ * Dynamic Programming
+ * Since we recalculate the optimal stack with bottom b[i] frequently, we can
+ * save the answers in a DP table (in this case a DP map from bottoms to
+ * optimal stacks) and look them up when necessary.
  */
 
 import java.util.*;
 
 public class Q10 {
 
+  /**
+   * Calculates the height of the stack
+   */
+  public static int height(Stack<Box> stk) {
+    int h = 0;
+    for(Box b : stk)
+      h += b.height;
+    return h;
+  }
+
+  /**
+   * Calculates the optimal stack with the specified Box at the bottom
+   */
+  public static Stack<Box> solve(Box[] boxes, Box bottom, Map<Box, Stack<Box>> dp) {
+
+    if(dp.containsKey(bottom)) {
+      return dp.get(bottom);
+    }
+     
+    int bestHeight = 0;
+    Stack<Box> best = null;
+
+    for(Box box : boxes) {
+      if(box.lessThan(bottom)) {
+        Stack<Box> stk = solve(boxes, box, dp);
+        int h = height(stk);
+        if(h > bestHeight) {
+          bestHeight = h;
+          best = stk;
+        }
+      }
+    }
+
+    if(best == null) {
+      best = new Stack<>();
+    }
+
+    best.push(bottom);
+
+    dp.put(bottom, (Stack<Box>)best.clone());
+    System.out.format("%s : %s\n", bottom, best);
+
+    return best;
+  }    
+
+  /**
+   * Input: starts with N, the number of boxes followed by N lines, each
+   * containing w, d and h separated by spaces.
+   */
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    Box[] boxes = new Box[n];
+    for(int i = 0; i < n; i++) {
+      int w = sc.nextInt();
+      int h = sc.nextInt();
+      int d = sc.nextInt();
+      boxes[i] = new Box(w, h, d);
+    }
+
+    int bestHeight = 0;
+    Stack<Box> best = null;
+
+    Map<Box, Stack<Box>> map = new HashMap<>();
+
+    for(Box box : boxes) {
+      Stack<Box> stk = solve(boxes, box, map);
+      int h = height(stk);
+      if(h > bestHeight) {
+        bestHeight = h;
+        best = stk;
+      }
+    }
+
+    System.out.println(best);
 
   }
 
 }
 
 class Box {
+  int width, height, depth;
+  public Box(int w, int h, int d) {
+    width = w;
+    height = h;
+    depth = d;
+  } 
+
+  public boolean lessThan(Box o) {
+    return width < o.width && height < o.height && depth < o.depth;
+  }
+
+  public String toString() {
+    return String.format("(%d %d %d)", width, height, depth);
+  }
 }
--- ch09/Q9.java
+++ ch09/Q9.java
@@ -1,13 +1,89 @@
 /*
+ * Write an algorithm to print all ways of arranging eight queens on an 8x8
+ * chess board so that none of them share the same row, column or diagonal. In
+ * this case, "diagonal" means all diagonals, not just the two that bisect the
+ * board.
  *
+ * My solution stores the board as a long, which means I can't do this for any
+ * chess board larger than 8 because I was initially storing all of the
+ * solutions in a HashSet and I thought a 64-bit long was the fastest
+ * way to check two boards are equal. It turns out that we don't need to do
+ * that. I'm keeping the solution using the longs because I think the bit
+ * manipulation is neat.
+ *
+ * My solution is to place a queen in each row (or column, I chose row) and
+ * check the constraints. I stored the constraints as three arrays: col, rdiag
+ * and ldiag. col stores which columns have been taken up, rdiag stores which
+ * right diagonals (diagonals going from the left up to the right) have been taken
+ * up and ldiag stores the left diagonals (diagonals going from left down to
+ * the right).
+ *
+ * To check which right diagonal a point (i, j) is on, we take i + j.
+ * To check which left diagonal a point (i, j) is on, we take the difference i
+ * - j then we add N - 1 as an offset for negative differences.
+ *
+ * I know this is not the most elegant solution, but it's at least more elegant
+ * than my initial solution which was to have a boolean array of spaces that
+ * are attacked. I did it this way before and the solution was not pretty! I
+ * had to have four separate for loops to "attack" diagonals.
  */
 
 import java.util.*;
 
 public class Q9 {
 
-  public static void main(String[] args) {
+  public static int count = 0;
 
+  public static void placeQueens(boolean[] col, boolean[] rdiag, boolean[] ldiag, long board, int row) { 
+    
+    if(row == 8) {
+      printBoard(board);
+      count++;
+      return;
+    }
+
+    for(int j = 0; j < 8; j++) {
+      long mask = 1L << (row * 8 + j);
+      if((board & mask) == 0 && !col[j] && !rdiag[row + j] && !ldiag[row - j + 7]) {
+        col[j] = true;
+        rdiag[row + j] = true;
+        ldiag[row - j + 7] = true;
+        // set bit to 1
+        board = board | mask;
+        placeQueens(col, rdiag, ldiag, board, row + 1);
+        // set bit to 0
+        board = board & ~mask;
+        col[j] = false;
+        rdiag[row + j] = false;
+        ldiag[row - j + 7] = false;
+
+      }
+    }
+  }
+
+  public static void printBoard(long board) {
+    long mask = 0;
+    for(int i = 0; i < 8; i++) {
+      for(int j = 0; j < 8; j++) {
+        mask = 1L << (i * 8 + j);
+        if((board & mask) != 0)
+          System.out.print("Q ");
+        else
+          System.out.print(". ");
+      }
+      System.out.println();
+    }
+    System.out.println("----------");
+  }
+
+
+  public static void main(String[] args) {
+    boolean[] col = new boolean[8];
+    boolean[] rdiag = new boolean[16];
+    boolean[] ldiag = new boolean[16];
+    long board = 0L;
+    placeQueens(col, rdiag, ldiag, board, 0);
+    System.out.println(count);
   }
 
 }
